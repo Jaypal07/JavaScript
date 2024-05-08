8c50bb0ff8e6328013beedfb43fa4c93c2983fc5
Jaypal Koli <69342922+Jaypal07@users.noreply.github.com>
2024-05-08 06:36:40 +0000

    Datatypes

--- Index.js
+++ Index.js
@@ -2,12 +2,17 @@ console.log("Jaypal");
 {
     var a = 6;
 }
+/* prefer not to use var
+beacause of issue in blocked and functional scope
+*/
 
 {
     let b = 7;
 }
-   
 
+const c = 123;
+// c = 44; not allowed 
 
 console.log(a);
+console.table([a,c]);
 // outside of scope --> console.log(b);
---
+++ dataTypes.js
@@ -0,0 +1,12 @@
+let name = "Jaypal" // string
+let age = 23;       // number
+let isLoggedIn = false; // boolean
+
+// null 
+// undefinded
+// symbol
+
+// object
+
+console.log(typeof undefined);
+console.log(typeof null)
