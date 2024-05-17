d0e24e7a84004dd2e6c1b444ad2560e96a962834
Jaypal Koli <69342922+Jaypal07@users.noreply.github.com>
2024-05-17 07:08:15 +0000

    Array 2

--- Array.js
+++ Array.js
@@ -1,8 +1,24 @@
 let arr = [1,2,3];
-console.log(arr);
+// console.log(arr);
 let ar = arr;
-console.log(ar);
+// console.log(ar);
 
 ar[0]="5"; // shallow copy
-console.log(ar);
-console.log(arr);
+// console.log(ar);
+// console.log(arr);
+
+const m = ['1', '2', '3'];
+const l = ['4', '5', '6'];
+
+// m.push(l);
+
+// console.log(m);
+// console.log(m[3]);
+
+const n= m.concat(l); // concat return new array
+
+console.log(n);
+
+let o = [...m, ...l, ...m];
+
+console.log(o);
